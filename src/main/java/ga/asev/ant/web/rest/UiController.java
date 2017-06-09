113ce98bf7597c05f380a78cd4b80626a8929071
aasevastianov <ant.sevastyanov@gmail.com>
2017-06-09 14:45:50 +0300

    fix

---
+++ src/main/java/ga/asev/ant/web/rest/UiController.java
@@ -0,0 +1,53 @@
+package ga.asev.ant.web.rest;
+
+import ga.asev.ant.dao.model.NotificationItem;
+import ga.asev.ant.dao.model.Rule;
+import ga.asev.ant.dao.model.User;
+import ga.asev.ant.notification.NotificationService;
+import ga.asev.ant.rule.RuleService;
+import ga.asev.ant.web.UserContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+public class UiController {
+
+    private RuleService ruleService;
+    private NotificationService notificationService;
+    private UserContext userContext;
+
+
+    public UiController(RuleService ruleService, NotificationService notificationService, UserContext userContext) {
+        this.ruleService = ruleService;
+        this.notificationService = notificationService;
+        this.userContext = userContext;
+    }
+
+    @RequestMapping(path = "/rule", method = POST)
+    public void addRule(Rule rule) {
+        ruleService.addRule(rule);
+    }
+
+    @RequestMapping(path = "/rule", method = DELETE)
+    public void deleteRule(Long ruleId) {
+        ruleService.deleteRule(ruleId);
+    }
+
+    @RequestMapping(path = "/user/rules")
+    public List<Rule> getUserRules() {
+        User user = userContext.getUser();
+        return ruleService.getUserRules(user.getId());
+    }
+
+    @RequestMapping(path = "/user/notifications")
+    public List<NotificationItem> getUserNotifications() {
+        User user = userContext.getUser();
+        return notificationService.getUserNotifications(user.getId());
+    }
+
+}
